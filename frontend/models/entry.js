ac8df61f6015f5558568e19649e2a603df805290
jessegan <jesse13579@gmail.com>
2020-09-11 12:28:04 -0400

    initialize entry model

---
+++ frontend/models/entry.js
@@ -0,0 +1,11 @@
+class Entry {
+
+    constructor(id,rating,note,date,time){
+        this.id=id
+        this.rating=rating
+        this.note=note
+        this.date=date
+        this.time=time
+    }
+
+}
