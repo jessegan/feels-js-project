13700453c1cd88a19d5cd241bdbb97d27ef2f879
jessegan <jesse13579@gmail.com>
2020-09-11 14:35:50 -0400

    add custom constructor and rendering methods

--- frontend/models/entry.js
+++ frontend/models/entry.js
@@ -1,6 +1,17 @@
 class Entry {
     static all = []
 
+    // CONSTRUCTORS
+
+    /**
+     * Entry constructor
+     * 
+     * @param {Integer} id 
+     * @param {Integer} rating 
+     * @param {String} note 
+     * @param {String} date 
+     * @param {String} time 
+     */
     constructor(id,rating,note,date,time){
         this.id=id
         this.rating=rating
@@ -26,6 +37,44 @@ class Entry {
         this.all.push(entry)
 
         return entry
+    }
+
+    /**
+     * Given an array of Entry data, create new Entry objects for each set of data
+     * 
+     * @param {Array<Object>} entriesData  array of Objects containing data to create Entries
+     */
+    static createEntries(entriesData){
+        entriesData.forEach(data => {
+            let attrs = data.attributes
+            Entry.create(attrs.id, attrs.rating, attrs.note, attrs.date, attrs.time)
+        })
+    }
+
+    // INSTANCE METHODS
+
+    /**
+     * Adds entry div to document
+     */
+    render(){
+        let div = document.createElement('div')
+        div.classList.add("entry-container")
+        div.innerHTML = this.rating
+
+        entriesList().appendChild(div)
+    }
+
+
+    // CLASS METHODS
+
+    /**
+     * Renders all Entry objects to the document
+     * 
+     */
+    static renderEntries(){
+        this.all.forEach(entry => {
+            entry.render()
+        })
     }
 
 }
