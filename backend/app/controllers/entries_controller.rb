000a49ad7a4e8113983cea13dfc00e43c36adbba
jessegan <jesse13579@gmail.com>
2020-09-11 10:54:13 -0400

    implement index action to render all entries

--- backend/app/controllers/entries_controller.rb
+++ backend/app/controllers/entries_controller.rb
@@ -2,6 +2,9 @@ class EntriesController < ApplicationController
 
     # renders the json for all entries
     def index
+        entries = Entry.all
+
+        render json: EntrySerializer.new(entries).serialized_json
     end
 
 end
