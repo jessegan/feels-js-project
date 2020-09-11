74aeaf1c1cd2b49f513212a1c01b3eba40fb23db
jessegan <jesse13579@gmail.com>
2020-09-11 11:49:13 -0400

    convert created_at into date and time attributes

--- backend/app/serializers/entry_serializer.rb
+++ backend/app/serializers/entry_serializer.rb
@@ -1,6 +1,14 @@
 class EntrySerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :rating, :note, :created_at
+  attributes :id, :rating, :note
+
+  attribute :date do |obj|
+    obj.created_at.in_time_zone('Eastern Time (US & Canada)').to_date
+  end
+
+  attribute :time do |obj|
+    obj.created_at.in_time_zone('Eastern Time (US & Canada)').to_s(:time)
+  end
 
 end
