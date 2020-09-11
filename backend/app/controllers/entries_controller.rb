9a7c38f4172b9f2a09845d7801329de29e3f8044
jessegan <jesse13579@gmail.com>
2020-09-11 10:40:39 -0400

    add index action

--- backend/app/controllers/entries_controller.rb
+++ backend/app/controllers/entries_controller.rb
@@ -1,2 +1,7 @@
 class EntriesController < ApplicationController
+
+    # renders the json for all entries
+    def index
+    end
+
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :entries
+
+  resources :entries, only: [:index]
+
 end
