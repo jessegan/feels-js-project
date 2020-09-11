3a082d0fde210db169b4d84a825c290a63903bd3
jessegan <jesse13579@gmail.com>
2020-09-11 09:56:58 -0400

    add rating and note validations, add default scope

--- backend/app/models/entry.rb
+++ backend/app/models/entry.rb
@@ -1,2 +1,14 @@
 class Entry < ApplicationRecord
+
+    ### ASSOCIATIONS
+
+    ### VALIDATIONS
+
+    validates :rating, presence: true
+    validates :note, length: {maximum: 250}
+
+    ### SCOPES
+
+    default_scope {order(created_at: :desc)}
+
 end
