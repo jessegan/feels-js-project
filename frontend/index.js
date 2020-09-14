f3e171a1911af94ab70c01105bbaee1671a2af56
jessegan <jesse13579@gmail.com>
2020-09-14 12:25:31 -0400

    add functions to create a date interval that updates current date

--- frontend/index.js
+++ frontend/index.js
@@ -7,10 +7,10 @@ document.addEventListener("DOMContentLoaded", callOnLoad)
 
 /**
  * Loads page content and add event listeners to key elements.
- * 
  */
 function callOnLoad(){
     loadEntries()
+    loadCurrentDate()
 }
 
 /**
@@ -25,4 +25,37 @@ function loadEntries(){
             Entry.renderEntries()
         })
         .catch(errors => console.log(errors))
+}
+
+/**
+ * Load the current date and time on the page and set timeout for changing time
+ */
+function loadCurrentDate(){
+    let date = dateInterval()
+
+    date.start()
+    setTimeout(date.stop,5*1000)
+}
+
+function dateInterval(){
+    let id
+
+    const start = () => {
+        id = setInterval(renderDate,1*1000)
+    }
+
+    const stop = () => {
+        clearInterval(id)
+        id = undefined
+    }
+
+    return {
+        start: start,
+        stop: stop
+    }
+}
+
+function renderDate(){
+    let today = new Date()
+    console.log(today)
 }
