e45f15e50c3b422b1a411c80370be43519337acd
jessegan <jesse13579@gmail.com>
2020-09-16 11:47:12 -0400

    add comma to date

--- frontend/index.js
+++ frontend/index.js
@@ -76,7 +76,7 @@ function renderDate(){
     let current = new Date()
 
     let date = document.createElement('h1')
-    date.innerHTML = `${current.toLocaleDateString('default', {weekday: 'long'})} ${current.toLocaleString('default', {month: 'short'})} ${current.getDate()}`
+    date.innerHTML = `${current.toLocaleDateString('default', {weekday: 'long'})}, ${current.toLocaleString('default', {month: 'short'})} ${current.getDate()}`
 
     let time = document.createElement('h3')
     time.innerHTML = `${current.toLocaleTimeString([],{hour12: true, hour: "numeric", minute: "2-digit", second: "2-digit"})}`
