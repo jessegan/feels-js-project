a1684aeef9c1cdb0f29be4426c40d30afc9bd8d6
jessegan <jesse13579@gmail.com>
2020-09-17 09:42:14 -0400

    create emotions table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_11_131938) do
+ActiveRecord::Schema.define(version: 2020_09_17_134017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "emotions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.integer "rating"
