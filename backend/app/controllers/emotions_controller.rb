7acc6c25f38643bba594c5f4ad853d24273bb19b
jessegan <jesse13579@gmail.com>
2020-09-18 11:07:26 -0400

    rename index action to entry and add index action

--- backend/app/controllers/emotions_controller.rb
+++ backend/app/controllers/emotions_controller.rb
@@ -1,6 +1,12 @@
 class EmotionsController < ApplicationController
 
     def index
+        emotions = Emotion.all
+
+        render json: EmotionSerializer.new(emotions).serialized_json
+    end
+
+    def entry
         emotions = Entry.find(params[:entry_id]).emotions
 
         render json: EmotionSerializer.new(emotions).serialized_json
