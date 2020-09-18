0bb268db693041fa6389406fcec94007b9534a98
jessegan <jesse13579@gmail.com>
2020-09-18 11:06:56 -0400

    add /emotions route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  get '/emotions', to: "emotions#index"
+
   resources :entries, only: [:index,:create,:destroy] do
-      get '/emotions', to: "emotions#index"
+      get '/emotions', to: "emotions#entry"
   end
 
 end
