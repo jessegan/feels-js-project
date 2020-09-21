61f44ca81281205a095f139bfb0f79ac4f25746a
jessegan <jesse13579@gmail.com>
2020-09-21 11:40:25 -0400

    add comments

--- frontend/models/emotion.js
+++ frontend/models/emotion.js
@@ -2,11 +2,26 @@ class Emotion {
 
     static all = []
 
+    /** CONSTRUCTORS */
+
+    /**
+     * Emotion constructor
+     * 
+     * @param {Int} id 
+     * @param {String} name 
+     */
     constructor(id,name){
         this.id = id
         this.name = name
     }
 
+    /**
+     * Create new Emotion object and persist in all
+     * @param {Int} id 
+     * @param {String} name 
+     * 
+     * @returns {Emotion} newly created Emotion object
+     */
     static create(id,name){
         const emotion = new Emotion(id,name)
 
@@ -15,6 +30,15 @@ class Emotion {
         return emotion
     }
 
+    /** CLASS METHODS */
+
+    /**
+     * Send GET request to retrieve all or a specific Emotion from API
+     * 
+     * @param {Int} id 
+     * 
+     * @returns {Promise} Promise to fetch Emotion data, promise result is array of Emotion objects
+     */
     static getEmotions(id=undefined){
         if (id){
             return fetch(`${baseURL}/entries/${id}/emotions`)
@@ -31,6 +55,14 @@ class Emotion {
         }
     }
 
+    /**
+     * Create new Emotion objects based on emotions data from API
+     * 
+     * @param {Array<Objet>} emotionsData 
+     * @param {Boolean} persist 
+     * 
+     * @returns {Array<Emotion>}
+     */
     static createEmotions(emotionsData, persist=false){
         let emotions = emotionsData.map(data => {
             let attrs = data.attributes
@@ -41,23 +73,38 @@ class Emotion {
         return emotions
     }
 
+    /**
+     * Find Emotion object in Emotion.all
+     * @param {Int} id 
+     * 
+     * @returns {Emotion}
+     */
     static find(id){
         return this.all.find(emotion => emotion.id == id)
     }
 
+    /**
+     * Resets emotions elements on entry form and then renders emotion elements
+     */
     static renderEmotionsOnForm(){
         emotionFormCheckboxes().innerHTML = ""
         emotionFormModalBody().innerHTML = ""
         emotionFormContainer().innerHTML = ""
 
+        // render elements on form and modal for each Emotion
         this.all.forEach(emotion => {
             emotion.renderFormInput()
             emotion.renderModal()
         })
     }
 
-    // INSTANCE METHODS
+    /** INSTANCE METHODS */ 
 
+    /**
+     * Create a div for Emotion on Entry modal
+     * 
+     * @returns {Node}
+     */
     createModalDiv(){
         let div = document.createElement('div')
         div.classList.add("col-sm-auto","border","rounded-pill","mx-1")
@@ -66,6 +113,11 @@ class Emotion {
         return div
     }
 
+    /**
+     * Create a div for Emotion on entry form
+     * 
+     * @returns {Node}
+     */
     createFormDiv(){
         let div = document.createElement('div')
         div.classList.add("col-sm-auto","border","rounded-pill","mx-1","my-1")
@@ -75,6 +127,9 @@ class Emotion {
         return div
     }
 
+    /**
+     * Create checkboxes for Emotion on entry form
+     */
     renderFormInput(){
         let checkboxes = emotionFormCheckboxes()
 
@@ -90,6 +145,9 @@ class Emotion {
         return input
     }
 
+    /**
+     * Create div for Emotion in emotions modal in entry form
+     */
     renderModal(){
         let modalBody = emotionFormModalBody()
 
